10a00334ce4d24d26974cb674173156ae210ffe4
eric2esca <eric2esca@gmail.com>
2018-09-19 02:33:44 -0500

    finished auth flow

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+// keys.js - figure out what set of credentails to return
+if(process.env.NODE_ENV === 'production'){
+  //We are in production - return the production set of keys
+  module.exports = require('./prod');
+} else{
+  //We are in development - return the dev keys!!!
+  module.exports = require('./dev');
+}
