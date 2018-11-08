6fb46dda6dbe204ac905893847640401bc137d62
eric2esca <eric2esca@gmail.com>
2018-11-08 02:19:23 -0600

    Users can see surveys

---
+++ client/src/components/surveys/formFields.js
@@ -0,0 +1,6 @@
+export default [
+    { label: 'Survey Title', name: 'title', noValueError: 'Provide a Survey Title' },
+    { label: 'Subject Line', name: 'subject', noValueError: 'Provide a Subject' },
+    { label: 'Email Body', name: 'body', noValueError: 'Provide a message for your customers' },
+    { label: 'Recipient List', name: 'recipients', noValueError: 'Please, enter valid emails!' }
+];
